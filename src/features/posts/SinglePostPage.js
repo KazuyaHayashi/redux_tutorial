4e6541eba4980a76062a8c23407839bec1b2a14e
Kazuya Hayashi <pumpkin.brownies@gmail.com>
2020-10-17 15:25:38 +0900

    Add edit post page

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import { Link } from 'react-router-dom';
+
+
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params;
 
@@ -19,6 +22,9 @@ export const SinglePostPage = ({ match }) => {
             <article className="post">
                 <h2>{post.title}</h2>
                 <p className="post-content">{post.content}</p>
+                <Link to={`/editPost/${postId}`} className="button">
+                    Edit Post
+                </Link>
             </article>
         </section>
     );
